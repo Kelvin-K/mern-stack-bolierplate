4bad36cd38bd95ccb384ef53acbaf44126054b0c
Kelvin Kamani <kamani_kelvin@yahoo.com>
2022-06-30 14:00:53 +0530

    Modified middleware and added snippet

--- client/src/store/middleware/loggerMiddleware.ts
+++ client/src/store/middleware/loggerMiddleware.ts
@@ -1,6 +1,8 @@
-const LoggerMiddleware = (store: any) => (next: any) => (action: any) => {
+import { Dispatch, MiddlewareAPI } from '@reduxjs/toolkit';
+
+const LoggerMiddleware = (store: MiddlewareAPI) => (dispatch: Dispatch) => (action: any) => {
 	console.log('dispatching', action);
-	return next(action);
+	return dispatch(action);
 }
 
 export default LoggerMiddleware;
