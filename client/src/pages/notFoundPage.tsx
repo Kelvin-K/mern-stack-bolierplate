6ae506b876a21d3c2130d1967c03fab7e9f7a23c
Kelvin Kamani <kamani_kelvin@yahoo.com>
2022-07-05 18:47:41 +0530

    Added not found page

--- client/src/pages/notFoundPage.tsx
+++ client/src/pages/notFoundPage.tsx
@@ -1,10 +1,15 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/notFoundPage.scss";
 
 export default class NotFoundPage extends Component {
 	render() {
 		return (
 			<div className="NotFoundPage">
-				The page you are looking for is not available.
+				<div className="code">404</div>
+				<div className="meaning">Not Found</div>
+				<div className="description">The resource requested could not be found on this server!</div>
+				<Link to="/" className="btn btn-primary goHomeButton">GO HOME</Link>
 			</div>
 		);
 	}
