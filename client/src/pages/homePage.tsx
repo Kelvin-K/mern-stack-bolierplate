b165c99ba8395bb6e12b2146c4d24819dd88329d
Kelvin Kamani <kamani_kelvin@yahoo.com>
2022-07-05 18:40:07 +0530

    Removed redundant code

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -14,19 +14,6 @@ class DispatchProps {
 
 export class HomePageComponent extends Component<StateProps & DispatchProps, any>
 {
-	componentDidMount() {
-		this.callApi()
-			.then(res => console.log(res))
-			.catch(err => console.log(err));
-	}
-
-	callApi = async () => {
-		const response = await fetch('/api/users');
-		const body = await response.json();
-
-		return body;
-	}
-
 	render() {
 		return (
 			<div className="HomePage">
